40da1d1252705edf86c74060b528e5853ecede51
Yadvir Jaswal <yadvirjaswal@gmail.com>
2024-12-13 11:43:41 +1300

    Verify search field visibility and type

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -6,15 +6,21 @@ namespace Practice_Basics_of_Playwright.Pages
     {
         private readonly IPage page;
         public readonly ILocator logo;
+        public readonly ILocator searchField;
         public HomePage(IPage page)
         {
             this.page = page;
             logo = page.GetByLabel("store logo");
-
+            searchField = page.Locator("#search");
         }
-        public async Task ClickOnLogo()
+        public async Task ClickOnLogoAsync()
         {
             await logo.ClickAsync();
+        }
+        public async Task<bool> IsSearchFieldInputElementAsync()
+        {
+            var searchFieldType = await searchField.GetAttributeAsync("type");
+            return searchFieldType == "text";
         }
         
     }
--- Tests/HomePageTests.cs
+++ Tests/HomePageTests.cs
@@ -17,11 +17,28 @@ namespace Practice_Basics_of_Playwright.Tests
             await Assertions.Expect(homePage.logo).ToBeEnabledAsync();
 
             // Act
-            await homePage.ClickOnLogo();
+            await homePage.ClickOnLogoAsync();
 
             // Assert
             await Assertions.Expect(page).ToHaveURLAsync(appSettings.BaseUrl);
             await Assertions.Expect(page).ToHaveTitleAsync("Home Page");
         }
+
+        [Fact]
+        public async Task SearchFieldShouldBeVisible()
+        {
+            // Arrange 
+            var homePage = new HomePage(page);
+
+            // Assert that search field is visible on page
+            await Assertions.Expect(homePage.searchField).ToBeVisibleAsync();
+
+            // Assert that search field is an input element
+            var isSearchFieldInputElement = await homePage.IsSearchFieldInputElementAsync();
+            Assert.True(isSearchFieldInputElement);
+        }
+
+
+
     }
 }
