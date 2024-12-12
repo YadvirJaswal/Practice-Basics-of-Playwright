2da723181517ce42e14ae1172671daa035d19742
Yadvir Jaswal <yadvirjaswal@gmail.com>
2024-12-13 11:15:16 +1300

    Verify the home page logo is visible and clickable

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
 
 namespace Practice_Basics_of_Playwright.Pages
 {
     public class HomePage
     {
         private readonly IPage page;
+        public readonly ILocator logo;
         public HomePage(IPage page)
         {
             this.page = page;
+            logo = page.GetByLabel("store logo");
+
         }
+        public async Task ClickOnLogo()
+        {
+            await logo.ClickAsync();
+        }
+        
     }
 }
---
+++ Tests/HomePageTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using Practice_Basics_of_Playwright.Core;
+using Practice_Basics_of_Playwright.Pages;
+
+namespace Practice_Basics_of_Playwright.Tests
+{
+    public class HomePageTests : BaseTest
+    {
+        [Fact]
+        public async Task LogoShouldBeVisisbleAndClickable()
+        {
+            // Arrange
+            var homePage = new HomePage(page);
+
+            // Assert
+            await Assertions.Expect(homePage.logo).ToBeVisibleAsync();
+            await Assertions.Expect(homePage.logo).ToBeEnabledAsync();
+
+            // Act
+            await homePage.ClickOnLogo();
+
+            // Assert
+            await Assertions.Expect(page).ToHaveURLAsync(appSettings.BaseUrl);
+        }
+    }
+}
